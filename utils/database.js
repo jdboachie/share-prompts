1ea557748dc86303256a05630f7f6b6b78c03e00
jdboachie <judeboachiekwame@gmail.com>
2023-11-06 20:44:19 +0000

    debug -->

--- utils/database.js
+++ utils/database.js
@@ -3,22 +3,24 @@ import mongoose from 'mongoose';
 let isConnected = false; // track the connection status
 
 export const connectToDatabase = async () => {
-    mongoose.set('strictQuery', true);
     if (isConnected) {
         console.log('=> using existing database connection');
         return;
-    } else {
-        try {
-            await mongoose.connect(process.env.MONGODB_URI, {
-                dbName: "promptshare",
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
-            isConnected = true;
-            console.log('=> using new database connection')
-        } catch (error) {
-            console.log("Jude Boachie Kwame error connecting to database")
-            console.log(error)
-        }
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "promptshare",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+        console.log('=> using new database connection');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        // Handle the error appropriately, e.g., rethrow it or exit the application.
     }
 }
+
+// Set strict mode for schema definition
+mongoose.set('strict', true);
