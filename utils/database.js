846b6936a2236d5520cb63638a4761511ad4a4ba
jdboachie <judeboachiekwame@gmail.com>
2023-11-07 22:30:17 +0000

    debug: server not printing existing connection usage

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,23 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; // track the connection status
+let cachedConnection = null;
 
 export const connectToDatabase = async () => {
-    mongoose.set('strictQuery', true);
-    if (isConnected) {
+    if (cachedConnection) {
         console.log('=> using existing database connection');
-        return;
+        return cachedConnection;
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        cachedConnection = await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "promptshare",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        isConnected = true;
         console.log('=> using new database connection');
+        return cachedConnection;
     } catch (error) {
         console.error('Error connecting to the database:', error);
-        // Handle the error appropriately, e.g., rethrow it or exit the application.
+        throw error;
     }
 }
