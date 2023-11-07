a46b23dd9266f30ba322ee1be8b51ab83f7d2f31
jdboachie <judeboachiekwame@gmail.com>
2023-11-07 01:01:28 +0000

    hotfix

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,14 +2,14 @@
 
 import { useState } from 'react'
 import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
 
     const { data: session } = useSession()
-    const Router = useRouter()
+    const router = useRouter()
 
     const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({
@@ -33,7 +33,7 @@ const CreatePrompt = () => {
             })
 
             if (response.ok) {
-                Router.push('/')
+                router.push('/')
             }
         } catch (error) {
             console.log(error)
